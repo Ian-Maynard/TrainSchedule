7a500a8be75cada8d1b657b78d07c9d10bcfed6b
Ian-Maynard <maynard.ian@gmail.com>
2017-04-06 21:34:04 -0400

    Multiple changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,43 +16,41 @@
     // Initial Values
     var namE = "";
     var desT = "";
+    var timE = 0;
     var freQ = 0;
     
     // Capture Button Click
     $("#add-user").on("click", function() {
-      // Don't refresh the page!
       event.preventDefault();
 
-      // YOUR TASK!!!
-      // Code in the logic for storing and retrieving the most recent user.
-      // Don't forget to provide initial data to your Firebase database.
       namE = $("#namE-input").val().trim();
       desT = $("#desT-input").val().trim();
+      timE = $("#timE-input").val().trim();
       freQ = $("#freQ-input").val().trim();
 
       database.ref().set({
         name: namE,
         destination : desT,
+        time : timE;
         frequency : freQ,
       });
 
     });
 
-    // Firebase watcher + initial loader HINT: .on("value")
+    // On click write to dataabse 
 
     database.ref().on("value", function(snapshot) {
-
-      // Log everything that's coming out of snapshot
       console.log(snapshot.val());
-      console.log(snapshot.val().name);
-      console.log(snapshot.val().email);
-      console.log(snapshot.val().age);
-      console.log(snapshot.val().comment);
+      console.log(snapshot.val().namE);
+      console.log(snapshot.val().desT);
+      console.log(snapshot.val().timE);
+      console.log(snapshot.val().freQ);
 
       // Change the HTML to reflect
-      $("#namE-display").html(snapshot.val().name);
-      $("#desT-display").html(snapshot.val().email);
-      $("#freQ-display").html(snapshot.val().age);
+      $("#namE-display").html(snapshot.val().namE);
+      $("#desT-display").html(snapshot.val().desT);
+      $("#timE-display").html(snapshot.val().timE);
+      $("#freQ-display").html(snapshot.val().freQ);
 
       // Handle the errors
     }, function(errorObject) {
