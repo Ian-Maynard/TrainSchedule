afbbbf47dc52d2b579961a025ee2f56d9cbba17a
Ian-Maynard <maynard.ian@gmail.com>
2017-04-09 15:49:46 -0400

    Multiple changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,7 @@
+$(document).ready(function(){
+
 // Init firebase 
+
   var config = {
     apiKey: "AIzaSyBD0FinT2IqcnBdGiOlJWDJZ0cxINq-xlQ",
     authDomain: "trainschedule-88339.firebaseapp.com",
@@ -7,40 +10,42 @@
     storageBucket: "trainschedule-88339.appspot.com",
     messagingSenderId: "609386761441"
   };
-    firebase.initializeApp(config);
-
-
+  firebase.initializeApp(config);
   var database = firebase.database();
-    // Initial Values
+
+    // Initialize Values
+
     var namE = "";
     var desT = "";
     var timE = 0;
     var freQ = 0;
+    var nArr = 0; 
+    var mAwa = 0; 
     
     // Capture Button Click
+
     $("#addTrain").on("click",function(event) {
       event.preventDefault();
-
       namE = $("#iName").val().trim();
       desT = $("#iDest").val().trim();
       timE = $("#iTime").val().trim();
       freQ = $("#iFreq").val().trim();
 
-      database.ref().push({
-        name: namE,
-        destination : desT, 
-        time : timE,
-        frequency : freQ,
-      });
+          database.ref().push({
+            name: namE,
+            destination : desT, 
+            time : timE,
+            frequency : freQ,
+          });
 
-      nuRow(namE,desT,timE,freQ); // Create New ROw 
+      newRow(namE,desT,timE,freQ); // Create New ROw 
 
     });
 
     // On click write to firebase
 
     database.ref().on("value", function(snapshot) {   // Firebase listener 
-      nuRow(snapshot.val().namE,snapshot.val().desT,snapshot.val().timE,snapshot.val().freQ);
+      newRow(snapshot.val().namE,snapshot.val().desT,snapshot.val().timE,snapshot.val().freQ);
     }, function(errorObject) {
       console.log("Errors handled: " + errorObject.code);
     });
@@ -49,20 +54,21 @@
 
 function timCalc(){
 
-}// Calculate time
-
+} // Calculate time
 
 function newRow(w,x,y,z){
-  var lnamE = w;
-  var ldesT = x;
-  var ltimE = y;
-  var lfreQ = z;
-  var nuRow=  $("<tr>");
-  var nuTd= $("<td");
+  var lnamE = "<tr><td>" + w + "</td>";
+  var ldesT = "<td>"+x+"</td>";
+  var ltimE = "<td>"+y+"</td>";
+  var lfreQ = "<td>"+z+"</td>";
+  var pHld1 = "<td>"+" "+"</td>";
+  var pHld2 = "<td>"+" "+"</td></tr>";
+  var nRow  = lnamE+ldesT+ltimE+lfreQ; // Build the row. 
 
   // Here call timCalc to calculate next arrival and Minutes away. 
-  nuRow = nuRow+nuTd+lnamE+nuTd+ldesT+nuTd+ltimE+nuTd+lfreQ+nuTd; // Build the row.
-  $("#trnTab").append(nuRow); // Append the row to the stable. 
+  
+  $("#trnTab").append(nRow); // Append the row to the table. 
 
 } // Add to table 
 
+});
